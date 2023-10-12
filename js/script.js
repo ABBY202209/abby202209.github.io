2433107e9b4deaab9c43b9b7f276f99b1b33570d
abby <abby201205@gmail.com>
2023-10-12 10:47:25 +0800

    更新資料

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // 跑馬燈
 var typed = new Typed(".typing", {
-    strings: ["", "learning web design..."],
+    strings: ["", "welcome to my portfolio!", "I'm learning web design..."],
     typeSpeed: 100,
     backspeed: 60,
     loop: true
